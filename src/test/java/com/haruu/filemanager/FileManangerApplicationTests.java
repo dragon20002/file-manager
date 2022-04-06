2253ecc7c44cb822937930ffc6918c6b15fb61bf
haruu <dragon20002@naver.com>
2022-04-06 17:38:05 +0900

    Spring Boot 버전업 중 오류 수정

--- src/test/java/com/haruu/filemanager/FileManangerApplicationTests.java
+++ src/test/java/com/haruu/filemanager/FileManangerApplicationTests.java
@@ -1,11 +1,8 @@
 package com.haruu.filemanager;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class FileManangerApplicationTests {
 
@@ -14,4 +11,3 @@ public class FileManangerApplicationTests {
 	}
 
 }
-
